150b863d0155496d1551ec063bf9f941e6714525
Kvyotho <matheuscetrangolo12@gmail.com>
2026-04-23 20:30:21 -0300

    Squashed 'POO-2/assessment1/' content from commit d37aaea
    git-subtree-dir: POO-2/assessment1
    git-subtree-split: d37aaea8e2357d8a1f1226016f09dcd660137fd1

---
+++ WSLN Quiz Game/quizGame/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace quizGame
+{
+    internal class Question
+    {
+
+        public string Text;
+        public string[] Answers;
+        public int CorrectAnswer;
+        public Image Picture;
+        public string Hint;
+    }
+}
